ed8a2fdf1058b82aeebf90bbfc00156ee4da4858
Rasika2135 <suryawanshirasika2135@gmail.com>
2024-02-18 18:04:58 +0530

    updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -84,7 +84,7 @@ const Main = () => {
             </button>
             <button className='border text-white border-gray-300 py-2 px-5 ml-4 rounded'>
            
-            My List
+            More info
             </button>
           </div>
           <p className='text-gray-400 text-sm'>
